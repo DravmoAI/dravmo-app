bdee3ab4275dec492354fe1583dc73c08ca22076
Rohan1279 <muniruddinrohan@gmail.com>
2025-07-24 21:31:23 +0600

    Fixes text alignments in features card

--- components/landing/how-it-works-section.tsx
+++ components/landing/how-it-works-section.tsx
@@ -61,7 +61,7 @@ export function HowItWorksSection() {
             height={800} 
             className="md:mt-[36px] mb-[69px] md:mb-[111px] size-[100px] md:size-[150px] mx-auto" 
           />
-          <div className="w-full flex items-center justify-between md:gap-x-[51px]">
+          <div className="w-full flex items-center justify-between">
             <p className="w-36 flex-1 md:w-[238px] font-quantico font-normal text-[12px] md:text-[16px] md:leading-[24px] md:tracking-[0.15em] text-white ml-[22px] md:ml-[34px]">
               Add design context for personalized results
             </p>
@@ -89,8 +89,8 @@ export function HowItWorksSection() {
             height={800} 
             className="md:mt-[36px] mb-[69px] md:mb-[111px] size-[100px] md:size-[150px] mx-auto" 
           />
-          <div className="flex items-center justify-between gap-x-[51px]">
-            <p className="w-32 md:w-[205px] font-quantico font-normal text-[12px] md:text-[16px] md:leading-[24px] md:tracking-[0.15em] text-white ml-[22px] md:ml-[34px]">
+          <div className="w-full flex items-center justify-between">
+            <p className="w-32 md:w-[235px] font-quantico font-normal text-[12px] md:text-[16px] md:leading-[24px] md:tracking-[0.15em] text-white ml-[22px] md:ml-[34px]">
               Generate actionable design improvements
             </p>
             <p className="md:-translate-y-[15px] font-krona-one font-normal text-[64px] md:text-[96px] leading-[24px] md:tracking-[0.15em] mr-4 md:mr-0" style={{ color: "rgba(91,213,175,0.16)" }}>
